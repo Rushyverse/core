c42aa492176a8a7cf3bc0144f3d57a0b318f6446
Distractic <distractic@outlook.fr>
2023-01-22 19:24:50 +0100

    tests: Add tests for add relation

--- src/test/kotlin/com/github/rushyverse/core/data/friend/FriendCacheServiceTest.kt
+++ src/test/kotlin/com/github/rushyverse/core/data/friend/FriendCacheServiceTest.kt
@@ -1,29 +1,18 @@
 package com.github.rushyverse.core.data.friend
 
 import com.github.rushyverse.core.cache.CacheClient
-import com.github.rushyverse.core.cache.CacheClient.Default.binaryFormat
 import com.github.rushyverse.core.container.createRedisContainer
 import com.github.rushyverse.core.data.FriendCacheService
 import com.github.rushyverse.core.data.UserCacheManager
-import com.github.rushyverse.core.data._Friends.Companion.friends
-import com.github.rushyverse.core.extension.toTypedArray
 import com.github.rushyverse.core.serializer.UUIDSerializer
-import com.github.rushyverse.core.utils.createKey
-import com.github.rushyverse.core.utils.getTTL
 import io.lettuce.core.RedisURI
 import io.lettuce.core.api.coroutines.RedisCoroutinesCommands
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.flow.toSet
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.builtins.serializer
-import kotlinx.serialization.protobuf.ProtoBuf
-import kotlinx.serialization.protobuf.ProtoBuf.Default.decodeFromByteArray
-import kotlinx.serialization.protobuf.ProtoBuf.Default.encodeToByteArray
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Timeout
@@ -31,8 +20,10 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.util.*
 import java.util.concurrent.TimeUnit
-import kotlin.test.*
-import kotlin.time.Duration.Companion.seconds
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
 
 @Timeout(5, unit = TimeUnit.SECONDS)
 @Testcontainers
@@ -64,7 +55,7 @@ class FriendCacheServiceTest {
     inner class AddFriend {
 
         @Test
-        fun `add relationship if relation is already in friends`() = runTest {
+        fun `add relationship if relation exists in friends`() = runTest {
             val uuid1 = UUID.randomUUID()
             val uuid2 = UUID.randomUUID()
 
@@ -90,6 +81,111 @@ class FriendCacheServiceTest {
             }
         }
 
+        @Test
+        fun `add relationship if relation doesn't exist in friends`() = runTest {
+            val uuid1 = UUID.randomUUID()
+            val uuid2 = UUID.randomUUID()
+
+            val cacheService = FriendCacheService(cacheClient, userCacheService)
+
+            assertTrue { cacheService.addFriend(uuid1, uuid2) }
+
+            cacheClient.connect {
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.FRIENDS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_FRIEND)).containsExactlyInAnyOrder(uuid2)
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.PENDING_REQUESTS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_PENDING_REQUEST)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+
+                FriendCacheService.Type.values().forEach { type ->
+                    assertThat(getAll(it, uuid2, type)).isEmpty()
+                }
+            }
+        }
+
+        @Test
+        fun `add relationship will delete relation in remove relationship`() = runTest {
+            val uuid1 = UUID.randomUUID()
+            val uuid2 = UUID.randomUUID()
+
+            val cacheService = FriendCacheService(cacheClient, userCacheService)
+
+            cacheClient.connect {
+                add(it, uuid1, uuid2, FriendCacheService.Type.REMOVE_FRIEND)
+            }
+
+            assertTrue { cacheService.addFriend(uuid1, uuid2) }
+
+            cacheClient.connect {
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.FRIENDS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_FRIEND)).containsExactlyInAnyOrder(uuid2)
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.PENDING_REQUESTS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_PENDING_REQUEST)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+
+                FriendCacheService.Type.values().forEach { type ->
+                    assertThat(getAll(it, uuid2, type)).isEmpty()
+                }
+            }
+        }
+
+        @Test
+        fun `add relationship will not change relation in pending requests`() = runTest {
+            val uuid1 = UUID.randomUUID()
+            val uuid2 = UUID.randomUUID()
+
+            val cacheService = FriendCacheService(cacheClient, userCacheService)
+
+            cacheClient.connect {
+                add(it, uuid1, uuid2, FriendCacheService.Type.PENDING_REQUESTS)
+            }
+
+            assertTrue { cacheService.addFriend(uuid1, uuid2) }
+
+            cacheClient.connect {
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.FRIENDS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_FRIEND)).containsExactlyInAnyOrder(uuid2)
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.PENDING_REQUESTS)).containsExactlyInAnyOrder(uuid2)
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_PENDING_REQUEST)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+
+                FriendCacheService.Type.values().forEach { type ->
+                    assertThat(getAll(it, uuid2, type)).isEmpty()
+                }
+            }
+        }
+
+        @Test
+        fun `add relation will not change relation from friend`() = runTest {
+            val uuid1 = UUID.randomUUID()
+            val uuid2 = UUID.randomUUID()
+
+            val cacheService = FriendCacheService(cacheClient, userCacheService)
+
+            cacheClient.connect {
+                FriendCacheService.Type.values().forEach { type ->
+                    add(it, uuid2, uuid1, type)
+                }
+            }
+
+            assertTrue { cacheService.addFriend(uuid1, uuid2) }
+
+            cacheClient.connect {
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.FRIENDS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_FRIEND)).containsExactlyInAnyOrder(uuid2)
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.PENDING_REQUESTS)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.ADD_PENDING_REQUEST)).isEmpty()
+                assertThat(getAll(it, uuid1, FriendCacheService.Type.REMOVE_FRIEND)).isEmpty()
+
+                FriendCacheService.Type.values().forEach { type ->
+                    assertThat(getAll(it, uuid2, type)).containsExactlyInAnyOrder(uuid1)
+                }
+            }
+        }
     }
 
     private suspend fun getAll(
@@ -97,7 +193,8 @@ class FriendCacheServiceTest {
         uuid1: UUID,
         type: FriendCacheService.Type
     ) =
-        it.smembers(encodeKeyWithType(uuid1, type)).map { cacheClient.binaryFormat.decodeFromByteArray(UUIDSerializer, it) }.toSet()
+        it.smembers(encodeKeyWithType(uuid1, type))
+            .map { cacheClient.binaryFormat.decodeFromByteArray(UUIDSerializer, it) }.toSet()
 
     private suspend fun add(
         connection: RedisCoroutinesCommands<ByteArray, ByteArray>,
