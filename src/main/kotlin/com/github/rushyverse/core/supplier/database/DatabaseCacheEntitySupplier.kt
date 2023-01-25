204a209f80ac5be7afe462d4e684fc6c1cbf0cd1
Distractic <distractic@outlook.fr>
2023-01-25 18:43:33 +0100

    chore: Duplication code  remove

--- src/main/kotlin/com/github/rushyverse/core/supplier/database/DatabaseCacheEntitySupplier.kt
+++ src/main/kotlin/com/github/rushyverse/core/supplier/database/DatabaseCacheEntitySupplier.kt
@@ -1,45 +1,11 @@
 package com.github.rushyverse.core.supplier.database
 
 import com.github.rushyverse.core.data.IFriendCacheService
-import kotlinx.coroutines.flow.Flow
-import java.util.*
 
 /**
  * [IDatabaseEntitySupplier] that uses cache to manage entities.
  * @property friendCacheService Friend cache service.
  */
-public class DatabaseCacheEntitySupplier(public val friendCacheService: IFriendCacheService) : IDatabaseEntitySupplier, IFriendCacheService by friendCacheService {
-
-    override suspend fun addFriend(uuid: UUID, friend: UUID): Boolean {
-        return friendCacheService.addFriend(uuid, friend)
-    }
-
-    override suspend fun addPendingFriend(uuid: UUID, friend: UUID): Boolean {
-        return friendCacheService.addPendingFriend(uuid, friend)
-    }
-
-    override suspend fun removeFriend(uuid: UUID, friend: UUID): Boolean {
-        return friendCacheService.removeFriend(uuid, friend)
-    }
-
-    override suspend fun removePendingFriend(uuid: UUID, friend: UUID): Boolean {
-        return friendCacheService.removePendingFriend(uuid, friend)
-    }
-
-    override suspend fun getFriends(uuid: UUID): Flow<UUID> {
-        return friendCacheService.getFriends(uuid)
-    }
-
-    override suspend fun getPendingFriends(uuid: UUID): Flow<UUID> {
-        return friendCacheService.getPendingFriends(uuid)
-    }
-
-    override suspend fun isFriend(uuid: UUID, friend: UUID): Boolean {
-        return friendCacheService.isFriend(uuid, friend)
-    }
-
-    override suspend fun isPendingFriend(uuid: UUID, friend: UUID): Boolean {
-        return friendCacheService.isPendingFriend(uuid, friend)
-    }
-}
-
+public class DatabaseCacheEntitySupplier(public val friendCacheService: IFriendCacheService) :
+    IDatabaseEntitySupplier,
+    IFriendCacheService by friendCacheService
