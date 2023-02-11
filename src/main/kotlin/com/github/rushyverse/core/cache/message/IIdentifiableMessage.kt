9053f3f88797a8532ed0550c56684fba9840307c
Distractic <distractic@outlook.fr>
2023-02-11 14:55:45 +0100

    feat: Add identifiable message

---
+++ src/main/kotlin/com/github/rushyverse/core/cache/message/IIdentifiableMessage.kt
@@ -0,0 +1,74 @@
+package com.github.rushyverse.core.cache.message
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.nullable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.*
+
+/**
+ * A message that can be identified by an id.
+ * @param T The type of the data.
+ * @property id ID of the message.
+ * @property data The data of the message.
+ */
+public interface IIdentifiableMessage<T> {
+    public val id: String?
+    public val data: T
+}
+
+/**
+ * A message that can be identified by an id.
+ * @param T The type of the data.
+ * @property id ID of the message.
+ * @property data The data of the message.
+ */
+public data class IdentifiableMessage<T>(override val id: String?, override val data: T) : IIdentifiableMessage<T>
+
+/**
+ * A serializer for [IdentifiableMessage].
+ * @param T The type of the data.
+ * @property dataSerializer The serializer for the [data field][IIdentifiableMessage.data].
+ */
+public class IdentifiableMessageSerializer<T>(private val dataSerializer: KSerializer<T>) : KSerializer<IIdentifiableMessage<T>> {
+
+    private val stringSerializer = String.serializer().nullable
+
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Message") {
+        element("id", stringSerializer.descriptor)
+        element("data", dataSerializer.descriptor)
+    }
+
+    override fun serialize(encoder: Encoder, value: IIdentifiableMessage<T>) {
+        encoder.encodeStructure(descriptor) {
+            encodeSerializableElement(descriptor, 0, stringSerializer, value.id)
+            encodeSerializableElement(descriptor, 1, dataSerializer, value.data)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): IIdentifiableMessage<T> {
+        return decoder.decodeStructure(descriptor) {
+            var id: String? = null
+            var data: T? = null
+
+            if(decodeSequentially()) {
+                id = decodeSerializableElement(descriptor, 0, stringSerializer)
+                data = decodeSerializableElement(descriptor, 1, dataSerializer)
+            } else {
+                while (true) {
+                    when (val index = decodeElementIndex(descriptor)) {
+                        0 -> id = decodeSerializableElement(descriptor, index, stringSerializer)
+                        1 -> data = decodeSerializableElement(descriptor, index, dataSerializer)
+                        CompositeDecoder.DECODE_DONE -> break
+                        else -> error("Unexpected index: $index")
+                    }
+                }
+            }
+
+            IdentifiableMessage(id = id, data = data ?: error("The field data is missing"))
+        }
+    }
+
+
+}
---
+++ src/test/kotlin/com/github/rushyverse/core/cache/message/IdentifiableMessageSerializerTest.kt
@@ -0,0 +1,124 @@
+package com.github.rushyverse.core.cache.message
+
+import com.github.rushyverse.core.utils.getRandomString
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.*
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@Serializable(with = ColorAsObjectSerializer::class)
+data class Color(val rgb: Int)
+
+object ColorAsObjectSerializer : KSerializer<Color> {
+
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("Color") {
+            element<Int>("r")
+            element<Int>("g")
+            element<Int>("b")
+        }
+
+    override fun serialize(encoder: Encoder, value: Color) =
+        encoder.encodeStructure(descriptor) {
+            encodeIntElement(descriptor, 0, (value.rgb shr 16) and 0xff)
+            encodeIntElement(descriptor, 1, (value.rgb shr 8) and 0xff)
+            encodeIntElement(descriptor, 2, value.rgb and 0xff)
+        }
+
+    override fun deserialize(decoder: Decoder): Color =
+        decoder.decodeStructure(descriptor) {
+            var r = -1
+            var g = -1
+            var b = -1
+            while (true) {
+                when (val index = decodeElementIndex(descriptor)) {
+                    0 -> r = decodeIntElement(descriptor, 0)
+                    1 -> g = decodeIntElement(descriptor, 1)
+                    2 -> b = decodeIntElement(descriptor, 2)
+                    CompositeDecoder.DECODE_DONE -> break
+                    else -> error("Unexpected index: $index")
+                }
+            }
+            require(r in 0..255 && g in 0..255 && b in 0..255)
+            Color((r shl 16) or (g shl 8) or b)
+        }
+
+}
+
+class IdentifiableMessageSerializerTest {
+
+    @Nested
+    inner class Serialize {
+
+        @Test
+        fun `should serialize message with id`() {
+            val serializer = IdentifiableMessageSerializer(String.serializer())
+            val message = IdentifiableMessage(null, "data")
+            val expected = "{\"id\":null,\"data\":\"data\"}"
+            val actual = Json.encodeToString(serializer, message)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun `should serialize message without id`() {
+            val serializer = IdentifiableMessageSerializer(String.serializer())
+            val id = getRandomString()
+            val message = IdentifiableMessage(id, "data")
+            val expected = "{\"id\":\"$id\",\"data\":\"data\"}"
+            val actual = Json.encodeToString(serializer, message)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun `should serialize message`() {
+            val serializer = IdentifiableMessageSerializer(ColorAsObjectSerializer)
+            val message = IdentifiableMessage(null, Color(0x05F0C1))
+            val expected = "{\"id\":null,\"data\":{\"r\":5,\"g\":240,\"b\":193}}"
+            val actual = Json.encodeToString(serializer, message)
+            assertEquals(expected, actual)
+        }
+
+    }
+
+    @Nested
+    inner class Deserialize {
+
+        @Test
+        fun `should deserialize message with id`() {
+            val serializer = IdentifiableMessageSerializer(String.serializer())
+            val id = getRandomString()
+            val data = getRandomString()
+            val message = IdentifiableMessage(id, data)
+            val expected = "{\"id\":\"$id\",\"data\":\"$data\"}"
+            val actual = Json.decodeFromString(serializer, expected)
+            assertEquals(message, actual)
+        }
+
+        @Test
+        fun `should deserialize message without id`() {
+            val serializer = IdentifiableMessageSerializer(String.serializer())
+            val message = IdentifiableMessage(null, "data")
+            val expected = "{\"id\":null,\"data\":\"data\"}"
+            val actual = Json.decodeFromString(serializer, expected)
+            assertEquals(message, actual)
+        }
+
+        @Test
+        fun `should deserialize message`() {
+            val serializer = IdentifiableMessageSerializer(ColorAsObjectSerializer)
+            val id = getRandomString()
+            val message = IdentifiableMessage(id, Color(0x05F0C1))
+            val expected = "{\"id\":\"$id\",\"data\":{\"r\":5,\"g\":240,\"b\":193}}"
+            val actual = Json.decodeFromString(serializer, expected)
+            assertEquals(message, actual)
+        }
+
+    }
+}
