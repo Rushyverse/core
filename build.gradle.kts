05f817c4832f3039ca9f7fbed40eabeadf8638d6
Distractic <distractic@outlook.fr>
2023-04-04 17:21:04 +0200

    chore: Set kotlin to 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
     id("org.jetbrains.dokka") version "1.8.10"
     id("com.google.devtools.ksp") version "1.8.20-1.0.10"
     `java-library`
