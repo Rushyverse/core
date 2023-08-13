69f746fb20a2435dd4c54cc08ee82f4e1db8d92d
Distractic <distractic@outlook.fr>
2023-08-13 12:10:10 +0200

    chore: Use stable version for mojang API

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     val junitVersion = "5.10.0"
     val testContainersVersion = "1.18.3"
     val lettuceVersion = "6.2.3.RELEASE"
-    val mojangApi = "2104df63d7"
+    val mojangApi = "v1.0.1"
     val nettyCodecVersion = "4.1.96.Final"
     val assertJcoreVersion = "3.24.2"
     val komapperVersion = "1.12.0"
