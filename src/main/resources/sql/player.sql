3b7ad129e469c7cab964d9958d16c5e933a7f63d
Distractic <distractic@outlook.fr>
2023-08-15 00:17:37 +0200

    fix(sql): Use varchar until Komapper not support enum

--- src/main/resources/sql/player.sql
+++ src/main/resources/sql/player.sql
@@ -1,10 +1,19 @@
+CREATE TYPE rank AS ENUM ('PLAYER', 'ADMIN');
 
--- CREATE TYPE rank AS ENUM ('PLAYER', 'ADMIN');
+CREATE TYPE language AS ENUM (
+    'ENGLISH',
+    'FRENCH',
+    'SPANISH',
+    'GERMAN',
+    'CHINESE'
+);
 
 -- Create table to store players
 CREATE TABLE player
 (
     uuid uuid PRIMARY KEY,
-    rank varchar(50) NOT NULL -- TODO: Change to ENUM when Komapper supports it
+    rank varchar(50) NOT NULL, -- TODO: Change to ENUM when Komapper supports it
 --     rank rank NOT NULL
+    language varchar(50) NOT NULL
+--     language language NOT NULL
 );
